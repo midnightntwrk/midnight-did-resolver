12270cbca07df271f3988f4c50e7314a7c6b368d
Pat Losoponkul <pat.losoponkul@iohk.io>
2025-09-22 16:55:00 +0700

    refactor: centralize macro definitions for compact types

--- midnight-did-serde/src/serde_impl/compact_v0_8.rs
+++ midnight-did-serde/src/serde_impl/compact_v0_8.rs
@@ -217,3 +217,80 @@ fn value_to_bigint(x: &ValueAtom) -> Result<BigInt, CompactError> {
     bytes.reverse();
     Ok(BigInt(bytes))
 }
+
+macro_rules! compact_enum {
+    ($name:ident { $($fields:ident),+ }) => {
+        #[allow(non_camel_case_types)]
+        #[derive(Debug, Clone)]
+        pub enum $name {
+            $($fields),+
+        }
+
+        impl CompactType for $name {
+            fn from_value(value: &mut Value) -> Result<Self, CompactError> {
+                let variants = [
+                    $(Self::$fields),+
+                ];
+                let idx = CompactTypeEnum::from_value(value)?.0;
+                variants.get(idx as usize).cloned().ok_or(CompactError(
+                    format!("exptected Enum[<=?] for type {}", stringify!($name)),
+                ))
+            }
+        }
+    };
+}
+
+macro_rules! compact_struct {
+    ($name:ident {
+        $($fields:ident: $tys:ty),+
+    }) => {
+        pub struct $name {
+            $($fields: $tys),+
+        }
+
+        impl CompactType for $name {
+            fn from_value(value: &mut Value) -> Result<Self, CompactError> {
+                $(let $fields = <$tys as CompactType>::from_value(value)?;)+
+                Ok(Self {
+                    $($fields),*
+                })
+            }
+        }
+    };
+}
+
+macro_rules! compact_ledger {
+    ($name:ident {
+        $($field:ident: $adt:tt <$ty:ty $(, $ty2:ty)?> [$($path:literal),*]),+
+    }) => {
+        paste::paste! {
+            $(
+                #[allow(non_camel_case_types)]
+                struct [<$name _ $field:camel>];
+
+                impl StateValuePath for [<$name _ $field:camel>]{
+                    fn state_value_path() -> &'static [u8] {
+                        &[$($path),*]
+                    }
+                }
+            )+
+
+            pub struct $name {
+                $($field: compact_ledger!(@internal $adt <$ty $(, $ty2)?>, [<$name _ $field:camel>])),+
+            }
+        }
+    };
+    (@internal cell <$ty:ty>, $pty:ident) => {
+        AdtTypeCell<$ty, $pty>
+    };
+    (@internal set <$ty:ty>, $pty:ident) => {
+        AdtTypeSet<$ty, $pty>
+    };
+    (@internal map <$ty:ty, $ty2:ty>, $pty:ident) => {
+        AdtTypeMap<$ty, $ty2, $pty>
+    }
+}
+
+pub(crate) use compact_ledger;
+pub(crate) use compact_struct;
+pub(crate) use compact_enum;
