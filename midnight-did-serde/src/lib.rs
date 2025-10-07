325738f57f17e10c666d1ad2211e372de01b5c4f
Pat Losoponkul <pat.losoponkul@iohk.io>
2025-10-07 21:54:10 +0700

    chore: update identus dependencies and refactor contract deserializer

--- midnight-did-serde/src/lib.rs
+++ midnight-did-serde/src/lib.rs
@@ -1,9 +1,3 @@
 mod serde_rs;
 
 pub use serde_rs::DefaultContractStateDeserializer;
-
-#[cfg(feature = "js-cli")]
-mod serde_cli;
-
-#[cfg(feature = "js-cli")]
-pub use serde_cli::CliContractStateDeserializer;
